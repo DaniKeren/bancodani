dfe9d903056a89e21469e44c4bd0f4febe7a2c06
DaniKeren <curraheexs@gmail.com>
2019-02-07 13:55:20 +0100

    modificacion

--- banco/PruebaBanco2/src/pruebabanco2/PruebaBanco2.java
+++ banco/PruebaBanco2/src/pruebabanco2/PruebaBanco2.java
@@ -17,6 +17,8 @@ public class PruebaBanco2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Banco");
+                System.out.println("Modificación");
+
     }
     
 }
